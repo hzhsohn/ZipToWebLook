1b217bef4d5bcc39c8c5fe98c8d8d905df3bc462
han.zh <han.zhihong@qq.com>
2017-06-13 18:20:43 +0800

    initizal

---
+++ helloProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloProject
+//
+//  Created by Han.zh on 2017/6/13.
+//  Copyright © 2017年 Han.zhihong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloProjectTests/helloProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  helloProjectTests.m
+//  helloProjectTests
+//
+//  Created by Han.zh on 2017/6/13.
+//  Copyright © 2017年 Han.zhihong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface helloProjectTests : XCTestCase
+
+@end
+
+@implementation helloProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
